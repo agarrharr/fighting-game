4981f17dff8f60d251f5f85308ed2015d4c84421
aharris88 / forbushbl <aharris88@gmail.com , forbushbl@gmail.com>
2015-01-08 07:34:01 -0700

    Pass the first 2 test

---
+++ game.js
@@ -0,0 +1,17 @@
+var game = function() {
+	
+	var board = function() {
+			
+	}();
+
+	var getPlayerLocations = function() {
+		return {
+			player1: 1,
+			player2: 15
+		};
+	};
+
+	return {
+		getPlayerLocations: getPlayerLocations
+	};
+}();
