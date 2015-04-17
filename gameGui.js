25b3661dfea407bc8c79727749c7ccbd2ab54090
Blaine Forbush <b.forbush@ldschurch.org>
2015-04-17 08:38:27 -0600

    Added board

--- gameGui.js
+++ gameGui.js
@@ -1,9 +1,16 @@
 d3.chart('gameBoard', {
 	initialize: function() {
+		var boardHeight = 200;
+		var boardWidth = 50;
+		var circleY = boardHeight / 2;
 		var circleRadius = 20;
-		var circleY = 50;
+		var playerColors = ['red', 'blue'];
+		var boardGroup = this.base.append('g').classed('boardGroup', true)
+				.attr('transform', 'translate(1,1)');
+		var circleGroup = this.base.append('g').classed('circleGroup', true)
+				.attr('transform', 'translate(1,1)');
 
-		this.layer('circles', this.base.append('g'), {
+		this.layer('circles', circleGroup, {
 			dataBind: function(data) {
 				return this.selectAll('circle')
 				.data(data);
@@ -17,15 +24,42 @@ d3.chart('gameBoard', {
 						'r': circleRadius,
 						'cy': circleY,
 						'cx': function(d) {
-							return xScale(d);
+							return xScale(d - 1) + (boardWidth / 2);
+						},
+						'fill': function(d, i) {
+							return playerColors[i];
 						}
 					});
 				}
 			}
 		});
 
-		var xScale = function(d) {
-			return d * 50;
+		this.layer('board', boardGroup, {
+			dataBind: function() {
+				return this.selectAll('rect')
+				.data([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
+			},
+			insert: function() {
+				return this.append('rect');
+			},
+			events: {
+				'enter': function() {
+					this.attr({
+						'height': boardHeight,
+						'width': boardWidth,
+						'y': 0,
+						'x': function(d, i) {
+							return xScale(i);
+						},
+						'fill': 'white',
+						'stroke': 'black'
+					});
+				}
+			}
+		});
+
+		var xScale = function(i) {
+			return i * boardWidth;
 		};
 	}
 });
